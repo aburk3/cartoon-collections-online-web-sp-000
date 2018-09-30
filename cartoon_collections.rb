ddceb722a615818083364e05aac0b3981b8f6145
Austin  Burke <aburk3@gmail.com>
2018-09-30 23:49:39 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,6 +19,6 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.detect do |maybe_cheese|
-    maybe_cheese.include?(cheese_types)
+    cheese_types.include?(maybe_cheese)
   end
 end
